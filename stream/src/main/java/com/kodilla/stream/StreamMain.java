2e1f274395bc664486ee4d535a60b970fde04edb
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2022-11-16 01:02:07 +0100

    7.3 added Forum and ForumUser class

--- stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,10 +1,9 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
-import com.kodilla.stream.person.People;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 
-import java.util.List;
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -13,34 +12,17 @@ public class StreamMain {
     public static void main (String[] args){
         System.out.println("Welcome to module 7 - Stream");
 
-        People.getList().stream()
-                .map(String::toUpperCase)
-                .filter(s -> s.length() > 11)
-                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
-                .filter(s -> s.charAt(0) == 'M')
+
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> theForumUsersResultMap = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getGender() == 'M')
+                .filter(forumUser -> forumUser.getBirthDate().getYear() <= (LocalDate.now().getYear() - 20))
+                .filter(forumUser -> forumUser.getPostCount() > 0)
+                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
+
+        System.out.println("# Elements: " + theForumUsersResultMap.size());
+        theForumUsersResultMap.entrySet().stream()
+                .map(entry->entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
-
-        BookDirectory theBookDirectory = new BookDirectory();
-        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005).toList();
-
-        System.out.println("# Elements: " + theResultListOfBooks.size());
-        theResultListOfBooks
-                .forEach(System.out::println);
-
-        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005).collect(Collectors.toMap(Book::getSignature, book -> book));
-
-        System.out.println("# Elements: " + theResultMapOfBooks.size());
-        theResultMapOfBooks.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-                .forEach(System.out::println);
-
-        String theResultStringOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .map(Book::toString)
-                .collect(Collectors.joining(",\n", "<<", ">>"));
-
-        System.out.println(theResultStringOfBooks);
     }
 }
--- stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
+++ stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
@@ -1,1 +1,51 @@
+package com.kodilla.stream.forumuser;
 
+import java.time.LocalDate;
+
+public class ForumUser {
+
+    private final int userID;
+    private final String username;
+    private final char gender;
+    private final LocalDate birthDate;
+    private final int postCount;
+
+    public ForumUser(int userID, String username, char gender, LocalDate birthDate, int postCount) {
+        this.userID = userID;
+        this.username = username;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.postCount = postCount;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public int getPostCount() {
+        return postCount;
+    }
+
+    @Override
+    public String toString() {
+        return "ForumUser{" +
+                "userID=" + userID +
+                ", username='" + username + '\'' +
+                ", gender=" + gender +
+                ", birthDate=" + birthDate +
+                ", postCount=" + postCount +
+                '}';
+    }
+}
