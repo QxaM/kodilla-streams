310b6e2549eb8c5856e7b4b872db0a7a857df89b
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2022-11-20 18:46:34 +0100

    7.5 variable name errors reworked

--- stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -11,29 +11,29 @@ public class WorldTestSuite {
     @Test
     void testGetPeopleQuantity(){
         //Given
-        World Earth = new World();
+        World earth = new World();
 
-        Continent Europe = new Continent();
-        Europe.addCountry(new Country("Poland", new BigDecimal("40000000")));
-        Europe.addCountry(new Country("France", new BigDecimal("67500000")));
-        Europe.addCountry(new Country("Sweden", new BigDecimal("10420000")));
+        Continent europe = new Continent();
+        europe.addCountry(new Country("Poland", new BigDecimal("40000000")));
+        europe.addCountry(new Country("France", new BigDecimal("67500000")));
+        europe.addCountry(new Country("Sweden", new BigDecimal("10420000")));
 
-        Continent Asia = new Continent();
-        Asia.addCountry(new Country("Japan", new BigDecimal("125700000")));
-        Asia.addCountry(new Country("China", new BigDecimal("12556330000")));
-        Asia.addCountry(new Country("Taiwan", new BigDecimal("23570000")));
+        Continent asia = new Continent();
+        asia.addCountry(new Country("Japan", new BigDecimal("125700000")));
+        asia.addCountry(new Country("China", new BigDecimal("12556330000")));
+        asia.addCountry(new Country("Taiwan", new BigDecimal("23570000")));
 
-        Continent SouthAmerica = new Continent();
-        SouthAmerica.addCountry(new Country("Salvador", new BigDecimal("6518000")));
-        SouthAmerica.addCountry(new Country("Argentina", new BigDecimal("45810000")));
-        SouthAmerica.addCountry(new Country("Peru", new BigDecimal("33360000")));
+        Continent southAmerica = new Continent();
+        southAmerica.addCountry(new Country("Salvador", new BigDecimal("6518000")));
+        southAmerica.addCountry(new Country("Argentina", new BigDecimal("45810000")));
+        southAmerica.addCountry(new Country("Peru", new BigDecimal("33360000")));
 
-        Earth.addContinent(Europe);
-        Earth.addContinent(Asia);
-        Earth.addContinent(SouthAmerica);
+        earth.addContinent(europe);
+        earth.addContinent(asia);
+        earth.addContinent(southAmerica);
 
         //When
-        BigDecimal resultPeopleQuantity = Earth.getPeopleQuantity();
+        BigDecimal resultPeopleQuantity = earth.getPeopleQuantity();
 
         //Then
         BigDecimal expectedPeopleQuantity = new BigDecimal("12909208000");
